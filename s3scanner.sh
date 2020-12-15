8f39958618fd3a1a5ceadc67773f0fa66d04b8b9
ryananicholson <ryananicholson@gmail.com>
2020-12-15 14:25:14 -0800

    Update s3scanner.sh

--- s3scanner.sh
+++ s3scanner.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-
 function usage {
     echo "USAGE: s3scanner.sh <file-with-bucket-names>"
     exit 1
 }
-
 if [ -z "$1" ]; then
     usage
 fi
-
 if ! [ -f $1 ]; then
     usage
 fi
-
 for i in $(cat $1); do
-    CODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}' http://$i.s3.amazonaws.com)
+    CODE=""
+    CODE=$(curl  -o out.txt --silent --head --write-out '%{http_code}' http://$i.s3.amazonaws.com)
     if [[ $CODE == "200" ]]; then
-        echo "$i.s3.amazonaws.com is public!"
+        echo -e "\033[31m$i.s3.amazonaws.com is public! \033[0m"
+    else
+        echo "$i.s3.amazonaws.com is not public!"
     fi
 done
